5d4366e159f959a54536b907f7ced9fc93ffa78f
zain ali <ibrahimkhan35821@gmail.com>
2025-12-18 00:23:54 +0100

    Configure Django Admin for Profile, Post, and Comment models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Comment   
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'author__username', 'content')
+    list_filter = ('status', 'created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    search_fields = ('post__title', 'author__username', 'content')
+    list_filter = ('created_at',)
+
