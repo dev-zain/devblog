07fc605fdee66f1b342112413e4a37fa6356b222
zain ali <ibrahimkhan35821@gmail.com>
2025-12-18 01:42:53 +0100

    Add blog views, URLs, and templates with Bootstrap styling

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(status='published').order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status='published')
+    comments = post.comments.all().order_by('created_at')
+    context = {
+        'post': post,
+        'comments': comments
+        }
+    return render(request, 'blog/post_detail.html', context)
+
